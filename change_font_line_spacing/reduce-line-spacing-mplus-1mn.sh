6a259a0a7ad0021de001ce0b5f068781b8454d1e
q1701 <q1701@outlook.com>
2017-12-10 16:29:37 +0900

    実行するための Shell script を追加

---
+++ change_font_line_spacing/reduce-line-spacing-mplus-1mn.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# M+ MN Type-1 - Thin
+./change_font_line_spacing.py \
+    ./font_in/mplus-1mn-thin.ttf \
+    ./font_out \
+    890 -305
+# M+ MN Type-1 - Light
+./change_font_line_spacing.py \
+    ./font_in/mplus-1mn-light.ttf \
+    ./font_out \
+    890 -305
+# M+ MN Type-1 - Regular
+./change_font_line_spacing.py \
+    ./font_in/mplus-1mn-regular.ttf \
+    ./font_out \
+    890 -305
+# M+ MN Type-1 - Medium
+./change_font_line_spacing.py \
+    ./font_in/mplus-1mn-medium.ttf \
+    ./font_out \
+    890 -305
+# M+ MN Type-1 - Bold
+./change_font_line_spacing.py \
+    ./font_in/mplus-1mn-bold.ttf \
+    ./font_out \
+    890 -305
