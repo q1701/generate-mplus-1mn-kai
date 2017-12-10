7dd139c81b19684a4e6ae87a9e3c3ccd0bbc9707
q1701 <q1701@outlook.com>
2017-12-10 16:21:04 +0900

    change_font_line_spacing を追加
    LICENSE も修正した

---
+++ change_font_line_spacing/change_font_line_spacing.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+The original version is:
+  http://blog.kaorimatz.org/2013/12/14/adjust-line-spacing-for-text-on-terminal.html
+"""
+
+import sys
+import os
+import fontforge
+
+
+def change_font_line_spacing(src_file, dest_dir, ascent, descent):
+    # type: (str, str, int, int) -> None
+    if not os.path.exists(src_file):
+        raise Exception('File not found: {}'.format(src_file))
+    if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir)
+    if not os.path.isdir(dest_dir):
+        raise Exception('\'{}\' must be a directory'.format(dest_dir))
+    font = fontforge.open(src_file)
+    font.os2_winascent = ascent
+    font.os2_winascent_add = False
+    font.os2_windescent = -descent
+    font.os2_windescent_add = False
+    font.hhea_ascent = ascent
+    font.hhea_ascent_add = False
+    font.hhea_descent = descent
+    font.hhea_descent_add = False
+    dest_file = os.path.basename(src_file)
+    font.generate(dest_file)
+    font.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print('usage: {} <source font file> <destination directory> <new ascent> <new descent>'.format(sys.argv[0]))
+    else:
+        change_font_line_spacing(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
