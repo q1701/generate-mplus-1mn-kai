2b8ef00a98d2010db5303abaa5ba89088811d318
q1701 <q1701@outlook.com>
2017-12-10 17:07:45 +0900

    change_font_line_spacing のディレクトリ名を変更, .gitignore を追加
    font_in, font_out => fonts_in, fonts_out

--- change_font_line_spacing/reduce-line-spacing-mplus-1mn.sh
+++ change_font_line_spacing/reduce-line-spacing-mplus-1mn.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # M+ MN Type-1 - Thin
-./change_font_line_spacing.py \
-    ./font_in/mplus-1mn-thin.ttf \
-    ./font_out \
+./change_fonts_line_spacing.py \
+    ./fonts_in/mplus-1mn-thin.ttf \
+    ./fonts_out \
     890 -305
 # M+ MN Type-1 - Light
-./change_font_line_spacing.py \
-    ./font_in/mplus-1mn-light.ttf \
-    ./font_out \
+./change_fonts_line_spacing.py \
+    ./fonts_in/mplus-1mn-light.ttf \
+    ./fonts_out \
     890 -305
 # M+ MN Type-1 - Regular
-./change_font_line_spacing.py \
-    ./font_in/mplus-1mn-regular.ttf \
-    ./font_out \
+./change_fonts_line_spacing.py \
+    ./fonts_in/mplus-1mn-regular.ttf \
+    ./fonts_out \
     890 -305
 # M+ MN Type-1 - Medium
-./change_font_line_spacing.py \
-    ./font_in/mplus-1mn-medium.ttf \
-    ./font_out \
+./change_fonts_line_spacing.py \
+    ./fonts_in/mplus-1mn-medium.ttf \
+    ./fonts_out \
     890 -305
 # M+ MN Type-1 - Bold
-./change_font_line_spacing.py \
-    ./font_in/mplus-1mn-bold.ttf \
-    ./font_out \
+./change_fonts_line_spacing.py \
+    ./fonts_in/mplus-1mn-bold.ttf \
+    ./fonts_out \
     890 -305
