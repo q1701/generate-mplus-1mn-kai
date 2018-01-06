2c965ec69172144b142211293c83e442e743be39
q1701 <q1701@outlook.com>
2018-01-07 00:41:32 +0900

    行間の余裕を縮小（75 -> 30）

--- change_font_line_spacing/reduce-line-spacing-mplus-1mn.sh
+++ change_font_line_spacing/reduce-line-spacing-mplus-1mn.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
+ascender_base=815
+descender_base=-230
+ascender_margin=30
+descender_margin=-30
+new_ascender=$((ascender_base+ascender_margin))
+new_descender=$((descender_base+descender_margin))
+
 # M+ MN Type-1 - Thin
 ./change_font_line_spacing.py \
     ./fonts_in/mplus-1mn-thin.ttf \
     ./fonts_out \
-    890 -305
+    ${new_ascender} ${new_descender}
 # M+ MN Type-1 - Light
 ./change_font_line_spacing.py \
     ./fonts_in/mplus-1mn-light.ttf \
     ./fonts_out \
-    890 -305
+    ${new_ascender} ${new_descender}
 # M+ MN Type-1 - Regular
 ./change_font_line_spacing.py \
     ./fonts_in/mplus-1mn-regular.ttf \
     ./fonts_out \
-    890 -305
+    ${new_ascender} ${new_descender}
 # M+ MN Type-1 - Medium
 ./change_font_line_spacing.py \
     ./fonts_in/mplus-1mn-medium.ttf \
     ./fonts_out \
-    890 -305
+    ${new_ascender} ${new_descender}
 # M+ MN Type-1 - Bold
 ./change_font_line_spacing.py \
     ./fonts_in/mplus-1mn-bold.ttf \
     ./fonts_out \
-    890 -305
+    ${new_ascender} ${new_descender}
